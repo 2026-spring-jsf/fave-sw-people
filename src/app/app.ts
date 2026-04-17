3b3115a1fd7bf2a0020bd407e89fd7dfa004eb45
tsteelematc <tsteele@madisoncollege.edu>
2026-04-17 11:39:13 -0500

    cleaning out default angular app stuffy-stuff

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('fave-sw-people');
+  protected readonly title = signal('Fave SW People');
 }
