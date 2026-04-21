4ab324b4e420df9a761308b012e8861abcde99f0
drew-weise <drewweise@gmail.com>
2026-04-21 10:35:26 -0500

    Client side navigation to my component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DweiseFaves } from './dweise-faves/dweise-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "dweise",
+        component: DweiseFaves,
+    },
+];
