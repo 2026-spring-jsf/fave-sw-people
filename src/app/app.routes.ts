1415cbe5c90ccba5ae0077f0029f8473285cbad2
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-25 05:41:27 -0500

    Implemented client-side nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CsniderFaves } from './csnider-faves/csnider-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "csnider",
+        component: CsniderFaves,
+    },
+];
