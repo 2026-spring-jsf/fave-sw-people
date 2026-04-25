577449af13cb571002001b4b7e5666325e482ea0
tsteelematc <tsteele@madisoncollege.edu>
2026-04-25 11:16:08 -0500

    showing sw people, phew....

--- src/app/tsteele-faves/tsteele-faves.ts
+++ src/app/tsteele-faves/tsteele-faves.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tsteele-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './tsteele-faves.html',
   styleUrl: './tsteele-faves.css',
 })
-export class TsteeleFaves {}
+export class TsteeleFaves {
+  private swPeopleSvc = inject(SwPeopleService);
+
+  protected readonly people$ = this.swPeopleSvc.getSwPeople();
+}
