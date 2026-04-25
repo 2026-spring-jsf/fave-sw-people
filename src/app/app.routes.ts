29bccb8c19a48c18bbc1ebd2c70ba518fe009e29
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-25 06:21:51 -0500

    Merged master and resolved conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,14 @@
 import { Routes } from '@angular/router';
-import { CsniderFaves } from './csnider-faves/csnider-faves';
-import { TsteeleFaves } from './tsteele-faves/tsteele-faves';
+
 import { CbaresFaves } from './cbares-faves/cbares-faves';
+import { CsniderFaves } from './csnider-faves/csnider-faves';
 import { DweiseFaves } from './dweise-faves/dweise-faves';
-import { ThamiltonFaves } from './thamilton-faves/thamilton-faves';
 import { LmeierFaves } from './lmeier-faves/lmeier-faves';
 import { MrooneyFaves } from './mrooney-faves/mrooney-faves';
+import { ThamiltonFaves } from './thamilton-faves/thamilton-faves';
+import { TsteeleFaves } from './tsteele-faves/tsteele-faves';
 
 export const routes: Routes = [
-    {
-        path: "tsteele",
-        component: TsteeleFaves,
-    },
     {
         path: "cbares",
         component: CbaresFaves,
@@ -25,15 +22,19 @@ export const routes: Routes = [
         component: DweiseFaves,
     },
     {
-        path: 'thamilton',
-        component: ThamiltonFaves,
-  },
-  {
         path: 'lmeier',
         component: LmeierFaves,
-  },
-  {
+    },
+    {
         path: 'mrooney',
         component: MrooneyFaves,
-  },
+    },
+    {
+        path: 'thamilton',
+        component: ThamiltonFaves,
+    },
+    {
+        path: "tsteele",
+        component: TsteeleFaves,
+    },
 ];
