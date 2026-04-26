715b524dfc0d673d30ca567d598be80bf615523a
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-04-26 15:33:25 -0500

    client side navigation to my component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ZsmuckerbryanFaves } from './zsmuckerbryan-faves/zsmuckerbryan-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"zsmuckerbryan",
+        component: ZsmuckerbryanFaves,
+    },
+];
