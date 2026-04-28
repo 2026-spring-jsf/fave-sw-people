6ead3cf71ed1a492e0fc13dbac5e6dd18783b982
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-28 13:10:27 -0500

    showing alphabetized named, neato...
    but rxjs is scary too

--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,12 @@ export class SwPeopleService {
     const fromApi = this.http.get<any>('https://swapi.info/api/people');
     //This was mostly for the purpose of ensuring we got the desired data
     //fromApi.subscribe((results) => console.log(results));
-    return fromApi;
+    return fromApi.pipe(
+      tap((x) => console.log(x)),
+      map((x) => x.map((y: any) => y.name)),
+      tap((x) => console.log(x)),
+      map((x) => x.sort((a: string, b: string) => a.localeCompare(b))),
+      tap((x) => console.log(x)),
+    );
   }
 }
