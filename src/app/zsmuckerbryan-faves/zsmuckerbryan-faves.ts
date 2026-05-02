4f9a3320761f7574537756d53b1bf6893aa0cefe
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-05-02 16:17:41 -0500

    .thenables and catches
    Not getting errors to work, but getting the "gross" true statements

--- src/app/zsmuckerbryan-faves/zsmuckerbryan-faves.ts
+++ src/app/zsmuckerbryan-faves/zsmuckerbryan-faves.ts
@@ -12,4 +12,25 @@ export class ZsmuckerbryanFaves {
   private swPeopleSvc = inject(SwPeopleService);
 
   protected readonly people$ = this.swPeopleSvc.getSwPeople();
+
+  protected promisesAsThenables() {
+    const numberPromise = this.swPeopleSvc.getMagicNumber(true)
+      .then(
+        n => {
+          console.log(n);
+
+          this.swPeopleSvc.getMagicNumber(true)
+          .then(
+            n2 => console.log(n2)
+          )
+          .catch (
+            e => console.warn(e)
+          )
+        ;
+      }
+    ).catch(
+      e => console.warn(e)
+    )
+    ;
+  }
 }
