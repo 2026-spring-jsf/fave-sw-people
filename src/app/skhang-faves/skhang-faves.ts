38c9a16e7a094fd54f0828ae1dedb77631e0f6e4
Shaun Khang <khaunx02@gmail.com>
2026-05-06 22:02:50 -0500

    ng generate component skhang-faves --skip-tests

---
+++ src/app/skhang-faves/skhang-faves.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skhang-faves',
+  imports: [],
+  templateUrl: './skhang-faves.html',
+  styleUrl: './skhang-faves.css',
+})
+export class SkhangFaves {}
