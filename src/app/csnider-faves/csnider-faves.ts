81a6f9c0095cfbc9b7c1492d5e597082108b947b
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-05-12 15:33:51 -0500

    Showing SW people from API

--- src/app/csnider-faves/csnider-faves.ts
+++ src/app/csnider-faves/csnider-faves.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+
 
 @Component({
   selector: 'app-csnider-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './csnider-faves.html',
   styleUrl: './csnider-faves.css',
 })
-export class CsniderFaves {}
+export class CsniderFaves {
+  private swPeopleService  = inject(SwPeopleService);
+
+  protected readonly people$ = this.swPeopleService.getSwPeople();
+}
