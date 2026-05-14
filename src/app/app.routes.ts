4cf4e9a7416592e9ef3feb6a2cfe7cc741c8560c
Kelsang Norbu <knorbu@edgewood.edu>
2026-05-14 16:53:26 -0500

    client side navigation to my component neatoooo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { KnorbuFaves } from './knorbu-faves/knorbu-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "knorbu",
+        component: KnorbuFaves,
+    },
+];
