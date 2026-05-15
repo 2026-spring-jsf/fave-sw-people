8423f53d515e7df36ce781ba764529fced841139
Kelsang Norbu <knorbu@edgewood.edu>
2026-05-15 13:36:43 -0500

    promises as thennables demo, gross

--- src/app/knorbu-faves/knorbu-faves.ts
+++ src/app/knorbu-faves/knorbu-faves.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-knorbu-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './knorbu-faves.html',
   styleUrl: './knorbu-faves.css',
 })
-export class KnorbuFaves {}
+export class KnorbuFaves {
+  private swPeopleSvc = inject(SwPeopleService)
+
+  protected readonly people$ = this.swPeopleSvc.getSwPeople();
+
+  protected promisesAsThenables() {
+    const numberPromise = this.swPeopleSvc.getMagicNumber(true)
+      .then(
+        n => {
+          console.log(n);
+
+            this.swPeopleSvc.getMagicNumber(false)
+              .then(
+                n2 => console.log(n2)
+              )
+              .catch (
+                e => console.warn(e)
+              )
+            ;
+          }
+        )
+    .catch(
+      e => console.warn(e)
+    )
+    ;
+  }
+}
